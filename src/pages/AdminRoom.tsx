8f7bd7ec0a6f8f34da0db919d159df8a12e863d9
Bruno Lopes <eusoubruno7@gmail.com>
2021-06-24 22:32:01 -0300

    feat: listing of questions, question likes, questions removing and  room close

---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,99 @@
+import { FormEvent, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import logoImg from "../assets/logo.svg";
+import deleteImg from "../assets/delete.svg";
+import { Button } from "../components/Button";
+import { Question } from "../components/Question";
+
+import { RoomCode } from "../components/RoomCode";
+import { useAuth } from "../hooks/useAuth";
+import { useRoom } from "../hooks/useRoom";
+import { database } from "../services/firebase";
+
+import "../styles/room.scss";
+
+type RoomParams = {
+  id: string;
+};
+
+export function AdminRoom() {
+  const { user } = useAuth();
+  const history = useHistory();
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+  const [newQuestion, setNewQuestion] = useState("");
+  const { questions, title } = useRoom(roomId);
+
+  async function handlSendQuestion(event: FormEvent) {
+    event.preventDefault();
+    if (newQuestion.trim() === "") {
+      return;
+    }
+    if (!user) {
+      throw new Error("You must be logged in");
+    }
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighLighted: false,
+      isAnswered: false,
+    };
+    await database.ref(`rooms/${params.id}/questions`).push(question);
+    setNewQuestion("");
+  }
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm("Tem certeza que deseja excluir essa pergunta?")) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  }
+  async function handleEndRoom() {
+    await database.ref(`rooms/${roomId}`).update({
+      endedAt: new Date(),
+    });
+    history.push("/");
+  }
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="letmeask" />
+          <div>
+            <RoomCode code={roomId} />
+            <Button isOutlined onClick={handleEndRoom}>
+              Encerrar sala
+            </Button>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+        </div>
+        <div className="question-list">
+          {questions.map((question) => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              >
+                <button
+                  type="button"
+                  onClick={() => {
+                    handleDeleteQuestion(question.id);
+                  }}
+                >
+                  <img src={deleteImg} alt="Remover pergunta" />
+                </button>
+              </Question>
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
+}
